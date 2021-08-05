815f4e3ecc272518236961bcc882248bb92ce536
Leif Andrew Christian <mtmusicandart@gmail.com>
2021-08-04 23:52:56 -0600

    first

---
+++ components/AppCSS.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+// ".App": {
+//   textAlign: center
+// },
+
+// ".App-logo": {
+//   height: 40px
+//   pointerEvents: none
+// },
+
+
+// ".App-header": {
+//   backgroundColor: #282c34
+//   minHeight: 100vh
+//   display: flex
+//   flexDirection: column
+//   alignItems: center
+//   justifyContent: center
+//   fontSize: calc(10px + 2vmin)
+//   color: white
+// },
+
+// "#formInput": {
+//   color: black
+// },
+
+// ".App-link": {
+//   color: #61dafb
+// }
+
+// ".App": { textAlign: 'center'}
+
+App: {
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: 'center'
+  },
+
+
+}
---
+++ components/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export class about extends Component {
+    render() {
+        return (
+            <Text>
+                fuck off!
+            </Text>
+        )
+    }
+}
+
+export default about
